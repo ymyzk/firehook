0eb16772132cfb464b593e185596f4a22dc0e650
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2019-03-08 00:10:58 +0900

    Improve tradfri plugin

--- plugins/tradfri.js
+++ plugins/tradfri.js
@@ -1,21 +1,23 @@
 const { AccessoryTypes, TradfriClient } = require("node-tradfri-client");
 
 class TradfriPlugin {
-  constructor({ host, securityCode }) {
+  constructor({ host, identity, psk }) {
     this.host = host;
-    this.securityCode = securityCode;
+    this.identity = identity;
+    this.psk = psk;
   }
 
   async run(arg) {
     try {
       const tradfri = new TradfriClient(this.host);
-      const { identity, psk } = await tradfri.authenticate(this.securityCode);
-      await tradfri.connect(identity, psk);
+      // const { identity, psk } = await tradfri.authenticate(this.securityCode);
+      await tradfri.connect(this.identity, this.psk);
       tradfri.on("device updated", async (device) => {
         if (device.type === AccessoryTypes[arg.type]) {
-          const duration = 1000 + (arg.duration ? arg.duration * 1000 : 0);
+          const duration = 1500 + (arg.transitionTime ? arg.transitionTime  * 1000 : 0);
           await tradfri.operateLight(device, arg.operation);
           setTimeout(() => tradfri.destroy(), duration);
+          return;
         }
       }).observeDevices();
     } catch (e) {
